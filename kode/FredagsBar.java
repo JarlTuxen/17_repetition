e6e13dccdac46a6bd2de00a3b0152a46541d9131
JarlTuxen <jart@kea.dk>
2018-09-24 09:05:34 +0200

    1. commit af Fredagsbar

---
+++ kode/FredagsBar.java
@@ -0,0 +1,36 @@
+public class FredagsBar
+{
+   //attributer
+   private int antalOel;
+   private int antalSodavand;
+   private double prisOel;
+   private double prisSodavand;
+   private String navn;
+   private boolean erAaben;
+   
+   //konstruktører
+   public FredagsBar()
+   {
+      this.antalOel = 0;
+      this.antalSodavand = 0;
+      this.prisOel = 0.0;
+      this.prisSodavand = 0.0;
+      this.navn = "";
+      this.erAaben = false;
+   }
+   
+   public FredagsBar(String navnPaaNyBar)
+   {
+      this.navn = navnPaaNyBar;
+      this.antalOel = 0;
+      this.antalSodavand = 0;
+      this.prisOel = 0.0;
+      this.prisSodavand = 0.0;
+      this.erAaben = false;
+   }
+   
+   //get/set metoder
+   
+   
+   //metoder
+}
